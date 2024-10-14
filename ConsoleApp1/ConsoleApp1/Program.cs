0f2ba8c3913ed5f19f62ea180ce4f4443261092e
Andreas <andreas3x3@protonmail.com>
2024-10-14 11:04:07 +0200

    test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Add());
+        }
+        public static int Add()
+        {
+            return 2 + 2;
+        }
+    }
+}
